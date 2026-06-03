5ac3ca96030adaf26e460c0476d5711b892a92f2
Michele Memoli <michele.memoli@gmail.com>
2026-06-03 19:07:50 +0100

    chore(app): linting

--- alchemy.run.ts
+++ alchemy.run.ts
@@ -1,24 +1,24 @@
 import * as Alchemy from "alchemy";
-import * as Effect from "effect/Effect";
 import * as Cloudflare from "alchemy/Cloudflare";
+import * as Effect from "effect/Effect";
 
 export const Website = Cloudflare.Vite("mmlngl-site", {
-	name: "mmlngl-site",
-	compatibility: { flags: ["nodejs_compat"] },
+  name: "mmlngl-site",
+  compatibility: { flags: ["nodejs_compat"] },
 });
 
 export type WebsiteEnv = Cloudflare.InferEnv<typeof Website>;
 
 export default Alchemy.Stack(
-	"mmlngl",
-	{
-		providers: Cloudflare.providers(),
-		state: Cloudflare.state(),
-	},
-	Effect.gen(function* () {
-		const website = yield* Website;
-		return {
-			websiteUrl: website.url.as<string>(),
-		};
-	}),
+  "mmlngl",
+  {
+    providers: Cloudflare.providers(),
+    state: Cloudflare.state(),
+  },
+  Effect.gen(function* () {
+    const website = yield* Website;
+    return {
+      websiteUrl: website.url.as<string>(),
+    };
+  }),
 );
